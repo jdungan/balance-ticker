8341205e0bf092345ffad9b2f22033f2a9c3a2d1
jdungan <johnadungan@gmail.com>
2014-10-27 13:05:01 -0500

    More bootstrapping, add daily average

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
+var ExtMath,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+
+ExtMath = (function(_super) {
+  __extends(ExtMath, _super);
+
+  function ExtMath() {
+    return ExtMath.__super__.constructor.apply(this, arguments);
+  }
+
+  ExtMath.truncate = function(x, precision) {
+    var scale, scales;
+    if (precision == null) {
+      precision = 0;
+    }
+    scales = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000];
+    scale = scales[precision];
+    return Math.round(x * scale) / scale;
+  };
+
+  return ExtMath;
+
+})(Math);
+
 $(document).ready(function() {
   var build, data_url, _sum;
   _sum = function(memo, num) {
@@ -5,10 +30,24 @@ $(document).ready(function() {
   };
   build = function(data) {
     console.log(data.contents.data);
-    return d3.select('#programs').selectAll('li').data(data.contents.data).enter().append("li").classed("list-group-item", true).append('p').text(function(d, i) {
-      return d.name;
-    }).append('p').text(function(d, i) {
-      return _.reduce(d.y, _sum, 0);
+    d3.select('#programs').selectAll('li').data(data.contents.data).enter().append("li").classed("list-group-item", true);
+    return d3.selectAll("li.list-group-item").each(function(d, i) {
+      var e;
+      e = d3.select(this);
+      e.append("div").classed("media", true);
+      e.append('h4').text(function(d, i) {
+        return d.name;
+      });
+      e.append('p').text(function(d, i) {
+        var sum;
+        sum = _.reduce(d.y, _sum, 0);
+        return "SUM: " + sum;
+      });
+      return e.append('p').text(function(d, i) {
+        var sum;
+        sum = _.reduce(d.y, _sum, 0);
+        return "AVG: " + ExtMath.truncate(sum / d.y.length, 2);
+      });
     });
   };
   data_url = 'http://whateverorigin.org/get?url=' + encodeURIComponent('https://plot.ly/~lippytak/184/balance-metrics-checks.json') + '&callback=?';
--- src/app.coffee
+++ src/app.coffee
@@ -1,3 +1,10 @@
+class ExtMath extends Math
+  @truncate = (x, precision = 0) ->
+    scales = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000]
+    scale = scales[precision]
+    Math.round(x * scale) / scale
+
+
 $(document).ready( ->
   # color = d3.scale.category10();
   # h= window.innerHeight
@@ -9,6 +16,7 @@ $(document).ready( ->
   #       width : w
   #     })
 
+
   _sum = (memo, num) -> memo + num
   
   build = (data)->
@@ -19,10 +27,24 @@ $(document).ready( ->
         .enter()
         .append("li")
           .classed("list-group-item",true)
-          .append('p')
-          .text( (d,i)-> d.name )
-          .append('p')
-          .text( (d,i)-> _.reduce d.y ,_sum, 0 )
+            
+      d3.selectAll("li.list-group-item")
+        .each (d,i)->
+          e = d3.select @
+          e.append("div")
+            .classed("media",true)
+          e.append('h4')
+            .text( (d,i)-> d.name )
+          e.append('p')
+            .text( (d,i)-> 
+              sum=_.reduce d.y ,_sum, 0 
+              "SUM: "+sum  
+            )
+          e.append('p')
+            .text( (d,i)-> 
+              sum=_.reduce d.y ,_sum, 0 
+              "AVG: " + ExtMath.truncate sum / d.y.length,2
+            )
     
   data_url ='http://whateverorigin.org/get?url=' + encodeURIComponent('https://plot.ly/~lippytak/184/balance-metrics-checks.json') + '&callback=?' 
   $.getJSON data_url ,build
